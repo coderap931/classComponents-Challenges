b40b8b16937313475ff713fc6a131236f4ef7d69
coderap931 <88391040+coderap931@users.noreply.github.com>
2022-01-13 12:50:33 -0500

    search challenge partially functional

--- src/Components/Search/SearchIndex.js
+++ src/Components/Search/SearchIndex.js
@@ -1,21 +1,37 @@
-import React from 'react';
-import {Input} from 'reactstrap';
- 
-const SearchIndex extends Component() {
-   this.state = {
-     things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards']
-   }
- }
+import React, { Component } from 'react';
+import { Input } from 'reactstrap';
 
- function searchFunction() {
- }
+export default class SearchIndex extends Component {
+  constructor() {
+    super();
+    this.state = {
+      things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards'],
+      results: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards'],
+      searchTerm: '',
+    };
+  }
 
- render() {
-     <div>
-       <Input placeholder='Search Here' />
-       <h3>Results:</h3>
-     </div>
- }
+  changeHandler(event) {
+    this.setState({
+      searchTerm: event.target.value,
+    });   
+    let newArray = this.state.things.filter(thing => thing.toLowerCase().includes(this.state.searchTerm.toLowerCase()));
+    this.setState({
+      results: newArray,
+    });
+  }
 
- 
-export SearchIndex;
+  render() {
+    return (
+      <div>
+        <Input type="text" value={this.state.searchTerm} placeholder='Search Here' onChange={(event) => this.changeHandler(event)} />
+        <h3>Results:</h3>
+        <ul>
+          {this.state.results.map((result) => {
+            return (<li>{result}</li>);
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
